83044de99378e518ca0bede8eab337eec879ba60
Adam Tran <adam@tdtran.cz>
2022-12-15 22:34:39 +0100

    Render categories template

--- build.py
+++ build.py
@@ -1,22 +1,42 @@
 from jinja2 import Environment, FileSystemLoader
 import shutil
-
+from data import tasks
+import os
 
 def makeEnv():
     file_loader = FileSystemLoader('src/templates')
     env = Environment(loader=file_loader)
     return env
 
-def render_categories_cs(save_path):
+def render_categories():
     env = makeEnv()
-    categories_template = env.get_template("cs/categories.jinja2")
+    # czech template
+    categories_template_cs = env.get_template("cs/categories.jinja2")
+    # english template
 
-    output = categories_template.render()
-    with open(save_path, mode="w", encoding="utf-8") as file_output:
-        file_output.write(output)
+    # output czech template
+    output_cs = categories_template_cs.render()
+    with open("dist/cs/index.html", mode="w", encoding="utf-8") as file_output:
+        file_output.write(output_cs)
+    #output english template
+
+def render_subcategories():
+    env = makeEnv()
+    # czech template
+    subcategories_template_cs = env.get_template("cs/subcategories.jinja2")
+
+    for type, type_tasks in tasks.items():
+        output_cs = subcategories_template_cs.render(type_tasks=type_tasks)
+        if not os.path.exists(f"dist/cs/{type}"):
+            os.makedirs(f"dist/cs/{type}")
+        with open(f"dist/cs/{type}/index.html", mode="w", encoding="utf-8") as file_output:
+            file_output.write(output_cs)
 
 
-render_categories_cs("dist/cs/index.html")
+
+
+render_categories()
+render_subcategories()
 
 shutil.rmtree("dist/js")
 shutil.copytree("src/js", "dist/js")
