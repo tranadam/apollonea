d11c45fe2a02507774c51136f0be10c632c236b1
Adam Tran <adam@tdtran.cz>
2022-12-16 23:23:54 +0100

    Render task pages

--- build.py
+++ build.py
@@ -32,11 +32,31 @@ def render_subcategories():
         with open(f"dist/cs/{type}/index.html", mode="w", encoding="utf-8") as file_output:
             file_output.write(output_cs)
 
+def render_tasks():
+    env = makeEnv()
+    tasks_template_cs = env.get_template("cs/task.jinja2")
 
+    for type, type_tasks in tasks.items():
+        for task in type_tasks:
+            with open("src/bbp_rovnobezky_bod_na_primce.txt", mode="r", encoding="utf-8") as file_steps:
+                pre_steps = file_steps.read()
+                pre_steps = pre_steps.split("\n")
+                steps = []
+                for step in pre_steps:
+                    if len(step) == 0:
+                        continue
+                    steps.append(step[step.index(".")+1:].strip())
+
+            output_cs = tasks_template_cs.render(task=task, steps=steps)
+            if not os.path.exists(f"dist/cs/{type}/{task.get_path_name_cs()}"):
+                os.makedirs(f"dist/cs/{type}/{task.get_path_name_cs()}")
+            with open(f"dist/cs/{type}/{task.get_path_name_cs()}/index.html", mode="w", encoding="utf-8") as file_output:
+                file_output.write(output_cs)
 
 
 render_categories()
 render_subcategories()
+render_tasks()
 
 shutil.rmtree("dist/js")
 shutil.copytree("src/js", "dist/js")
