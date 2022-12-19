5175859374bb74a72b96b250c4ec612efbcc0f64
Adam Tran <adam@tdtran.cz>
2022-12-19 17:31:33 +0100

    Change template structure

--- build.py
+++ build.py
@@ -2,6 +2,8 @@ from jinja2 import Environment, FileSystemLoader
 import shutil
 from data import tasks
 import os
+import glob
+import json
 
 def makeEnv():
     file_loader = FileSystemLoader('src/templates')
@@ -10,20 +12,30 @@ def makeEnv():
 
 def render_categories():
     env = makeEnv()
-    # czech template
-    categories_template_cs = env.get_template("cs/categories.jinja2")
-    # english template
+    categories_template = env.get_template("categories.jinja2")
 
-    # output czech template
-    output_cs = categories_template_cs.render()
+    languages = {}
+
+    translation_paths = glob.glob("src/translations/categories*.json")
+
+    for translation_path in translation_paths:
+        filename = os.path.basename(translation_path)
+        lang_code = filename.split(".")[0].split("_")[1]
+
+        with open(f"src/translations/{filename}", mode="r", encoding="utf-8") as translation_file:
+            languages[lang_code] = json.load(translation_file)
+
+    output_cs = categories_template.render(**languages["cs"])
     with open("dist/cs/index.html", mode="w", encoding="utf-8") as file_output:
         file_output.write(output_cs)
-    #output english template
+    output_en = categories_template.render(**languages["en"])
+    with open("dist/en/index.html", mode="w", encoding="utf-8") as file_output:
+        file_output.write(output_en)
 
 def render_subcategories():
     env = makeEnv()
     # czech template
-    subcategories_template_cs = env.get_template("cs/subcategories.jinja2")
+    subcategories_template_cs = env.get_template("subcategories.jinja2")
 
     for type, type_tasks in tasks.items():
         output_cs = subcategories_template_cs.render(type_tasks=type_tasks)
@@ -34,7 +46,7 @@ def render_subcategories():
 
 def render_tasks():
     env = makeEnv()
-    tasks_template_cs = env.get_template("cs/task.jinja2")
+    tasks_template_cs = env.get_template("task.jinja2")
 
     for type, type_tasks in tasks.items():
         for task in type_tasks:
